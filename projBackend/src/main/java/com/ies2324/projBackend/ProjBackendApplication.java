545aabdd0a60328273b84ab5fc2568bf154fdee1
notjoao1 <joao.p.dourado1@gmail.com>
2023-11-21 15:00:18 +0000

    feat(backend): empty spring backend with dependencies

---
+++ projBackend/src/main/java/com/ies2324/projBackend/ProjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ies2324.projBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjBackendApplication.class, args);
+	}
+
+}
---
+++ projBackend/src/test/java/com/ies2324/projBackend/ProjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ies2324.projBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
