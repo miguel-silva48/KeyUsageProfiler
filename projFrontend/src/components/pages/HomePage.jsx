4e78306b1157f8746f7dbb439814fde962eed020
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2023-11-28 15:59:26 +0000

    imp(frontend/homepage): Create team start

--- projFrontend/src/components/pages/HomePage.jsx
+++ projFrontend/src/components/pages/HomePage.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { fetchData } from "../../utils";
 
 import typing_image from "../../assets/home_typing_image.png";
 
@@ -10,16 +8,34 @@ import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
   return (
     <div>
       <Navbar />
-      <div id="body" className="mx-[5%]">
+      <div className="flex w-screen flex-col items-start gap-2.5">
+        <div className="w-full h-[63rem]">
+          <div className="w-[35rem] h-[31.5rem] absolute left-[9.5rem] top-[14.4rem]">
+            <p className="w-[34rem] font-sans text-6xl not-italic font-extrabold leading-[4.125rem] bg-gradient-to-b from-[#6941C6] to-[#27164F] bg-clip-text text-transparent">
+              A service made for teams and groups of friends.
+            </p>
+            <p className="w-[31.37rem] text-base text-gray-600">Join the community and start tracking your statistics as well!</p>
+            <div className="flex items-center gap-3 self-stretch p-5">
+              <p>You can join a team through an Invite Link</p>
+            </div>
+            <form className="w-[34.5rem] h-[4.3rem] shrink-0">
+              <div className="flex w-full h-full flex-col justify-center items-start gap-2.5 shrink-0">
+                <div className="flex p-5 pr-6 items-center gap-3 flex-[1_0_0] self-stretch rounded-2xl border border-gray-400 bg-gray-50">
+                <input id="team-name" placeholder="Enter team name"></input>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* <div id="body" className="mx-[5%] mt-3">
         <h2 className="text-4xl font-bold">Welcome to Key Usage Profiler</h2>
 
-        <div className="container flex">
-            {/* Coluna de Texto */}
+        <div className="container flex"> */}
+            {/* Coluna de Texto
             <div className="w-1/2 p-6">
               <p className="text-xl">
                 A service made for teams and groups of friends.
@@ -30,13 +46,17 @@ const HomePage = () => {
             </div>
 
             {/* Coluna da Imagem */}
-            <div className="w-1/2 p-6">
-              <img className="mx-auto w-[50%] my-10" src={typing_image} alt="Typing" />
+            {/* <div className="w-1/2 p-6">
+              <img className="mx-auto w-[50%]" src={typing_image} alt="Typing" />
+            </div>
+            <div className="">
+              <p>You can join a team through an Invite Link</p>
+              <p>or</p>
+              <p>You can create a team and invite your friends!</p>
             </div>
           </div>
-
         <div className="divider"></div>
-      </div>
+      </div> */} */
       <Footer />
     </div>
   );
