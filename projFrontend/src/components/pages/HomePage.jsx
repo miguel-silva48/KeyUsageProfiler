63d2c55ba3ff893b34b9f754c4b461f57c1488d5
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2023-12-02 15:22:13 +0000

    imp(frontend/createteam_14): Rough create team

--- projFrontend/src/components/pages/HomePage.jsx
+++ projFrontend/src/components/pages/HomePage.jsx
@@ -1,5 +1,3 @@
-import { useNavigate } from "react-router-dom";
-
 import typing_image from "../../assets/home_typing_image.png";
 
 import "./../../utils/styles.css";
@@ -12,7 +10,7 @@ const HomePage = () => {
     <div>
       <Navbar />
       <div className="flex w-screen flex-col items-start gap-2.5">
-        <div className="w-full h-[63rem]">
+        <div className="w-full h-[63rem] bg-white">
           <div className="w-[35rem] h-[31.5rem] absolute left-[9.5rem] top-[14.4rem]">
             <p className="w-[34rem] font-sans text-6xl not-italic font-extrabold leading-[4.125rem] bg-gradient-to-b from-[#6941C6] to-[#27164F] bg-clip-text text-transparent">
               A service made for teams and groups of friends.
@@ -24,39 +22,19 @@ const HomePage = () => {
             <form className="w-[34.5rem] h-[4.3rem] shrink-0">
               <div className="flex w-full h-full flex-col justify-center items-start gap-2.5 shrink-0">
                 <div className="flex p-5 pr-6 items-center gap-3 flex-[1_0_0] self-stretch rounded-2xl border border-gray-400 bg-gray-50">
-                <input id="team-name" placeholder="Enter team name"></input>
+                  <input id="team-name" placeholder="Enter team name"></input>
+                  <button className="flex w-[13rem] h-[3.35rem] p-4 flex-col justify-center items-center gap-2.5 shrink-0 rounded-[0.625rem] bg-gray-950">
+                    <div className="flex justify-center items-center gap-2">
+                      <p className="text-white text-base font-bold text-white">Join Team</p>
+                    </div>
+                  </button>
                 </div>
               </div>
             </form>
           </div>
         </div>
+        <img className="w-[22rem] h-[22rem] absolute right-80 top-56" src={typing_image} alt="Typing" />
       </div>
-      {/* <div id="body" className="mx-[5%] mt-3">
-        <h2 className="text-4xl font-bold">Welcome to Key Usage Profiler</h2>
-
-        <div className="container flex"> */}
-            {/* Coluna de Texto
-            <div className="w-1/2 p-6">
-              <p className="text-xl">
-                A service made for teams and groups of friends.
-              </p>
-              <p className="text-sm">
-                Join the community and start tracking your keystrokes as well!
-              </p>
-            </div>
-
-            {/* Coluna da Imagem */}
-            {/* <div className="w-1/2 p-6">
-              <img className="mx-auto w-[50%]" src={typing_image} alt="Typing" />
-            </div>
-            <div className="">
-              <p>You can join a team through an Invite Link</p>
-              <p>or</p>
-              <p>You can create a team and invite your friends!</p>
-            </div>
-          </div>
-        <div className="divider"></div>
-      </div> */} */
       <Footer />
     </div>
   );
