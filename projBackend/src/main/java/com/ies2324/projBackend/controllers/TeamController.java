5ef63e70c85ee3e1ea0022bf4ed5d5e56abb56d5
Migas77 <miguel.belchior@ua.pt>
2023-12-05 09:35:36 +0000

    Feat(TeamController): Get a user's team (updated team)

--- projBackend/src/main/java/com/ies2324/projBackend/controllers/TeamController.java
+++ projBackend/src/main/java/com/ies2324/projBackend/controllers/TeamController.java
@@ -1,9 +1,12 @@
 package com.ies2324.projBackend.controllers;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -53,5 +56,13 @@ public class TeamController {
         return ResponseEntity.ok(res);
     }
 
+    @GetMapping("user")
+    public ResponseEntity<Team> getUserTeam() {
+        // necessary because of new team members
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (user.getRole() == Role.USER)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(user.getTeam(), HttpStatus.OK);
+    }
 
 }
