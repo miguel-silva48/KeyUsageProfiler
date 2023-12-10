5867b517de52898e7585ddc4f1da5e5854d99d4b
notjoao1 <joao.dourado1@ua.pt>
2023-12-10 23:14:11 +0000

    hotfix(backend): regular users cant get their statistics

--- projBackend/src/main/java/com/ies2324/projBackend/controllers/UserStatisticsController.java
+++ projBackend/src/main/java/com/ies2324/projBackend/controllers/UserStatisticsController.java
@@ -6,8 +6,11 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import com.ies2324.projBackend.entities.Role;
+import com.ies2324.projBackend.entities.User;
 import com.ies2324.projBackend.entities.UserStatistics;
 import com.ies2324.projBackend.services.UserStatisticsService;
 
@@ -21,7 +24,8 @@ public class UserStatisticsController {
   @GetMapping("{id}")
   public ResponseEntity<UserStatistics> getUserStatisticsByAuthorId(@PathVariable("id") Long userId) {
     Optional<UserStatistics> userStatistics = userStatisticsService.getUserStatisticsByAuthorId(userId);
-    if (userStatistics.isEmpty())
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getRole() == Role.USER || userStatistics.isEmpty())
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     return new ResponseEntity<>(userStatistics.get(), HttpStatus.OK);
   }
