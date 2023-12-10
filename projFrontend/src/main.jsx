02ae5c61fc2abcea8843b4cb06f523754136eb29
notjoao1 <joao.dourado1@ua.pt>
2023-12-10 23:10:08 +0000

    hotfix(frontend): removed strict mode

--- projFrontend/src/main.jsx
+++ projFrontend/src/main.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import App from './App'
+import App from "./App";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
