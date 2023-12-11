cd38cb80ea9b5c4709a7286bd44c15091a9e9b0d
notjoao1 <joao.p.dourado1@gmail.com>
2023-12-11 11:50:16 +0000

    hotfix(backend): don't throw exception on keystroke receive

--- projBackend/src/main/java/com/ies2324/projBackend/consumer/KeystrokesConsumer.java
+++ projBackend/src/main/java/com/ies2324/projBackend/consumer/KeystrokesConsumer.java
@@ -1,6 +1,8 @@
 package com.ies2324.projBackend.consumer;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,15 +35,19 @@ public class KeystrokesConsumer {
     public void receive(byte[] in) throws JsonProcessingException, JsonMappingException {
         Keystroke k = mapper.readValue(new String(in, StandardCharsets.UTF_8), Keystroke.class);
         keystrokeSaver.addKeyStroke(String.valueOf(k.getAuthor().getId()), k);
-        User author = userService.getUserById(k.getAuthor().getId())
-                .orElseThrow(() -> new IllegalArgumentException("KeystrokeConsumer: User not found"));
-        Team team = author.getTeam();
-        k.setAuthor(author);
-        if (team != null) {
-            simpMessagingTemplate.convertAndSendToUser(
-                    team.getLeader().getUsername(),
-                    "/topic/keystrokes",
-                    k);
+        Optional<User> optAuthor = userService.getUserById(k.getAuthor().getId());
+        if (optAuthor.isPresent()) {
+            User author = optAuthor.get();
+            Team team = author.getTeam();
+            k.setAuthor(author);
+            if (team != null) {
+                simpMessagingTemplate.convertAndSendToUser(
+                        team.getLeader().getUsername(),
+                        "/topic/keystrokes",
+                        k);
+                System.out.printf("sent to this guy %s: %s\n", team.getLeader().getUsername(), k);
+            }
         }
+
     }
 }
