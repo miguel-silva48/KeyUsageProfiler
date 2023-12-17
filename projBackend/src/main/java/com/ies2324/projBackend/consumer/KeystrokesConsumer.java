808b2c4b454e33dd05359df59a640bcb8b1ebc4d
Migas77 <miguel.belchior@ua.pt>
2023-12-17 15:46:47 +0000

    Refactor(Receiver): Only save keystroke if user is in a team

--- projBackend/src/main/java/com/ies2324/projBackend/consumer/KeystrokesConsumer.java
+++ projBackend/src/main/java/com/ies2324/projBackend/consumer/KeystrokesConsumer.java
@@ -36,11 +36,11 @@ public class KeystrokesConsumer {
         Keystroke k = mapper.readValue(new String(in, StandardCharsets.UTF_8), Keystroke.class);
         Optional<User> optAuthor = userService.getUserById(k.getAuthor().getId());
         if (optAuthor.isPresent()) {
-            keystrokeSaver.addKeyStroke(String.valueOf(k.getAuthor().getId()), k);
             User author = optAuthor.get();
             Team team = author.getTeam();
-            k.setAuthor(author);
             if (team != null) {
+                keystrokeSaver.addKeyStroke(String.valueOf(k.getAuthor().getId()), k);
+                k.setAuthor(author);
                 simpMessagingTemplate.convertAndSendToUser(
                         team.getLeader().getUsername(),
                         "/topic/keystrokes",
